ccb0763829342febe71e5a6790961036b27ed9df
Sterbirk <tuong11051998@gmail.com>
2020-01-14 21:32:06 +0700

    update day1 lan 2

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trang Chu</title>
+    <link rel="stylesheet" href="/CP1896M04-PHP/assets/vendor/bootstrap/css/bootstrap.min.css">
+<style>
+div{
+    border: 5px solid red;
+
+
+}
+</style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+            Logo
+            </div>
+            <div class="col-md-9">
+            Ten Cong Ty 
+            </div>
+        </div><!--end row1-->
+        <div class="row">
+            <div class="col-md-12">
+                MENU
+            </div>
+        </div><!--end row2-->
+        <div class="row">
+            <div class="col-md-8">
+                content
+            </div>
+            <div class="col-md-4">
+                bar
+            </div>
+        </div><!--end row3-->
+        <div class="row">
+            <div class="col-md-3">
+                1
+            </div>
+            <div class="col-md-3">
+                2
+            </div>
+            <div class="col-md-3">
+                3
+            </div>
+            <div class="col-md-3">
+                4
+            </div>
+    </div>
+</body>
+</html>
